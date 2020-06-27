019784007a1cae59fb51500dc102cd13dec7e14a
Rabia Alhaffar <steria773@gmail.com>
2020-06-27 03:18:16 +0300

    Updated build to v0.0.2
    What's new?
    Removed pancake.device.online() cause of incompatibility
    
    Always see Changelog from documentation to know more

--- build/pancake.js
+++ build/pancake.js
@@ -345,17 +345,6 @@ pancake.device = {};
 pancake.device.screen_height = screen.height;
 pancake.device.screen_width = screen.width;
 
-pancake.device.online = function() {
-  var xhr = new (window.ActiveXObject || XMLHttpRequest)("Microsoft.XMLHTTP");
-  xhr.open("HEAD", "//" + window.location.hostname + "/?rand=" + Math.floor((1 + Math.random()) * 0x10000), false);
-  try {
-    xhr.send();
-    return (xhr.status >= 200 && (xhr.status < 300 || xhr.status === 304));
-  } catch (error) {
-    return false;
-  }
-};
-
 pancake.device.vibrate = function(pattern) {
     window.navigator.vibrate(pattern);
 };
