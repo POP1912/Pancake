d9a5a02cd3228543b35cd3bd37ce4a191c037528
Rabia Alhaffar <steria773@gmail.com>
2020-06-27 03:22:02 +0300

    Updated build to v0.0.2

--- build/pancake.js
+++ build/pancake.js
@@ -2,7 +2,7 @@
 // Copyright (c) 2020 - 2021 Rabia Alhaffar,Licensed under MIT License
 // Build Date: 27/June/2020
 var pancake = {};
-pancake.version = "v0.0.1";
+pancake.version = "v0.0.2";
 console.info("Made with Pancake " + pancake.version + "\nhttps://github.com/Rabios/Pancake");
 
 pancake.browser = {};
